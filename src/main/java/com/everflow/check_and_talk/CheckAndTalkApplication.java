dd39fcd3baecc984d8173395a50c1f293c76c102
gunyu1019 <gunyu1019@gmail.com>
2023-05-24 19:36:49 +0900

    [feat] init commit

---
+++ src/main/java/com/everflow/check_and_talk/CheckAndTalkApplication.java
@@ -0,0 +1,13 @@
+package com.everflow.check_and_talk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckAndTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckAndTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/everflow/check_and_talk/CheckAndTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everflow.check_and_talk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckAndTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
